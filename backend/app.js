e800e46fac267a49ca611eb78962209e7a860d2e
Diego <136228756+DicitoCarrera@users.noreply.github.com>
2025-02-18 15:12:27 +0100

    added backend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRoutes'); // Separate routes into their own file
-const { connectDB } = require('../database/db');  // Import MongoDB connection
-
-dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/usersRoutes"); // Separate routes into their own file
+const lessonsRouter = require("./routes/lessonsRoutes");
+const { connectDB } = require("./database/db"); // Import MongoDB connection
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+dotenv.config();
 
 // Middleware
 app.use(cors());
@@ -16,16 +17,19 @@ app.use(express.json()); // For parsing JSON requests
 // Connect to MongoDB
 connectDB()
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     process.exit(1); // Stop the server if DB connection fails
   });
 
-
 // Routes
-app.use('/api', userRoutes);  // Load user-related routes
+
+// Load user-related routes
+app.use("/api/users", userRoutes);
+// Use lessons routes
+app.use("/api/lessons", lessonsRouter);
 
 // Start the server
 app.listen(port, () => {
--- backend/database/db.js
+++ backend/database/db.js
@@ -1,7 +1,9 @@
 const { MongoClient } = require('mongodb');
+
 require('dotenv').config();
 
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017'; // Default local MongoDB URL
+const url = process.env.MONGODB_URI || 'mongodb://root:rootpassword@mongodb:27017'; // Default local MongoDB URL
+
 const client = new MongoClient(url);
 
 let db = null;
@@ -9,8 +11,7 @@ let db = null;
 const connectDB = async () => {
   try {
     await client.connect();
-    console.log('Connected to MongoDB');
-    db = client.db('myDatabase'); // Specify your database name here
+    db = client.db('WebEngDB'); // Specify your database name here
   } catch (error) {
     console.error('MongoDB connection error:', error);
   }
