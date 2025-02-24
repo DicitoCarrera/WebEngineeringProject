b1bf6b115417f3b005cdc39d477caa3cc4092d0a
Diego <136228756+DicitoCarrera@users.noreply.github.com>
2025-02-24 19:54:32 +0100

    added test protected router

--- backend/backend.js
+++ backend/backend.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-// const db = require("./database/lessons/lessonsDB");
 const authRoutes = require("./routes/authRoutes");
 const lessonsRoutes = require("./routes/lessonsRoutes");
 
-const app = express();
+const cookieParser = require('cookie-parser');
 const port = 5000;
 
+
+const app = express();
+
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(session({
@@ -16,6 +21,26 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }, // Set to true if using HTTPS
 }));
+
+// Protected route
+app.get('/protected', (req, res) => {
+  // Check if user is authenticated
+  if (req.session.user) {
+    res.send('Welcome to the protected area');
+  } else {
+    res.status(401).send('Unauthorized access');
+  }
+});
+
+
+// Middleware function
+const logRequest = (req, res, next) => {
+  console.log(`Received a ${req.method} request from ${req.ip}`);
+  next();
+};
+
+// Use the middleware
+app.use(logRequest);
 
 app.use("/auth", authRoutes);
 app.use("/lessons", lessonsRoutes);
