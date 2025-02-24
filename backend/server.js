a6460bbffbc7844ecea69a10e5af55850d051891
Diego <136228756+DicitoCarrera@users.noreply.github.com>
2025-02-24 18:42:21 +0100

    Clean all repo

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,32 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const db = require('./database');
-const userDb = require('./database/userDatabase');
-const authRoutes = require('./routes/authRoutes');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const db = require("./database/lessons/lessonsDB");
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 const port = 3000;
 
 // Установите Pug как шаблонизатор
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, '../frontend/views'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "../frontend/views"));
 
 // Убедитесь, что маршрут для главной страницы идет перед статическими файлами
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.use(session({
-  secret: 'your_secret_key',
+  secret: "your_secret_key",
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false } // Set to true if using HTTPS
+  cookie: { secure: false }, // Set to true if using HTTPS
 }));
 
-app.get('/lessons', (req, res) => {
+app.get("/lessons", (req, res) => {
   db.all("SELECT * FROM lessons", [], (err, rows) => {
     if (err) {
       res.status(500).send(err.message);
@@ -38,7 +36,7 @@ app.get('/lessons', (req, res) => {
   });
 });
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- frontend/assets/js/monacoSetup.js
+++ frontend/assets/js/monacoSetup.js
@@ -1,41 +1,46 @@
-require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.30.1/min/vs' }});
+require.config({
+  paths: {
+    "vs": "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.30.1/min/vs",
+  },
+});
 
-require(['vs/editor/editor.main'], function() {
-    const editorContainer = document.getElementById('editor-container');
-    const languageSelector = document.getElementById('language-selector');
-    let editor;
+require(["vs/editor/editor.main"], function () {
+  const editorContainer = document.getElementById("editor-container");
+  const languageSelector = document.getElementById("language-selector");
+  let editor;
 
-    function createEditor(language) {
-        if (editor) {
-            editor.dispose();
-        }
-        editor = monaco.editor.create(editorContainer, {
-            value: [
-                'function helloWorld() {',
-                '\tconsole.log("Hello, world!");',
-                '}'
-            ].join('\n'),
-            language: language,
-            theme: 'vs-dark'
-        });
+  function createEditor(language) {
+    if (editor) {
+      editor.dispose();
     }
+    editor = monaco.editor.create(editorContainer, {
+      value: [
+        "function helloWorld() {",
+        '\tconsole.log("Hello, world!");',
+        "}",
+      ].join("\n"),
+      language: language,
+      theme: "vs-dark",
+    });
+  }
 
-    if (editorContainer) {
-        createEditor('javascript');
+  if (editorContainer) {
+    createEditor("javascript");
 
-        languageSelector.addEventListener('change', function() {
-            const selectedLanguage = languageSelector.value;
-            createEditor(selectedLanguage);
-        });
+    languageSelector.addEventListener("change", function () {
+      const selectedLanguage = languageSelector.value;
+      createEditor(selectedLanguage);
+    });
 
-        document.getElementById('run-code').addEventListener('click', function () {
-            const code = editor.getValue();
-            try {
-                eval(code);
-                document.getElementById('output').innerText = 'Code executed successfully!';
-            } catch (error) {
-                document.getElementById('output').innerText = 'Error: ' + error.message;
-            }
-        });
-    }
+    document.getElementById("run-code").addEventListener("click", function () {
+      const code = editor.getValue();
+      try {
+        eval(code);
+        document.getElementById("output").innerText =
+          "Code executed successfully!";
+      } catch (error) {
+        document.getElementById("output").innerText = "Error: " + error.message;
+      }
+    });
+  }
 });
