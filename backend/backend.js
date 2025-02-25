d7eb0d0782c2c1a95e7d796a858a1fa31f8c7764
Warlockbarky <a.maslov1602@gmail.com>
2025-02-25 16:33:06 +0100

    switched port to 5001 and fixed some bugs

--- backend/backend.js
+++ backend/backend.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const session = require("express-session");
-const authRoutes = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const usersRoutes = require("./routes/usersRoutes");
 const lessonsRoutes = require("./routes/lessonsRoutes");
 
 const cookieParser = require('cookie-parser');
-const port = 5000;
-
+const port = 5001;
 
 const app = express();
 
 app.use(cookieParser());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+<<<<<<< HEAD
 // Log when the server starts
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
@@ -25,6 +26,42 @@ const lessonsRoutes = require("./routes/lessonsRoutes");
 
 // Routes
 app.use("/users", usersRoutes);
+=======
+// Настройка CORS для разрешения всех источников
+app.use(cors({
+  origin: 'http://localhost:3000', // Разрешить все источники
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Разрешить все методы
+  allowedHeaders: ['Content-Type', 'Authorization'] // Разрешить заголовки
+}));
+
+app.use(session({
+  secret: "your_secret_key",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }, // Set to true if using HTTPS
+}));
+
+// Protected route
+app.get('/protected', (req, res) => {
+  // Check if user is authenticated
+  if (req.session.user) {
+    res.send('Welcome to the protected area');
+  } else {
+    res.status(401).send('Unauthorized access');
+  }
+});
+
+// Middleware function
+const logRequest = (req, res, next) => {
+  console.log(`Received a ${req.method} request from ${req.ip}`);
+  next();
+};
+
+// Use the middleware
+app.use(logRequest);
+
+app.use("/auth", usersRoutes);
+>>>>>>> 1148b0f (switched port to 5001 and fixed some bugs)
 app.use("/lessons", lessonsRoutes);
 
 // General Error Handling
@@ -43,4 +80,8 @@ app.get("/", (req, res) => {
 app.use((req, res) => {
   console.log(`Unhandled route accessed: ${req.method} ${req.originalUrl}`);
   res.status(404).send({ error: "Not Found" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
